126e04be08b63a50e55200fb25e20ab1c842e72a
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-12-09 15:58:23 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ProfileContextProvider from "@/components/context/context";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <ProfileContextProvider>
         {children}
+        <Analytics/>
         </ProfileContextProvider>
       </body>
     </html>
